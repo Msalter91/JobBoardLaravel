4889065d4fb0da76dee29f7ee29fa3f20c3fe4ef
msalter <mike.salter@wundermanthompson.com>
2023-09-13 14:18:12 +0100

    data added to card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,5 @@
-{{--Attributes merge allows us to have a default set of classes (or any attribute that we can then stick on them) and it will merge this with the x-component declaration --}}
-<div {{$attributes->merge(['class'=>'rounded-md border-2 border-slate-300 bg-white p-4 shadow-sm'])}}>
+{{--Attributes merge allows us to have a default set of classes
+(or any attribute that we can then stick on them) and it will merge this with the x-component declaration --}}
+<article {{$attributes->merge(['class'=>'rounded-md border-2 border-slate-300 bg-white p-4 shadow-sm'])}}>
     {{ $slot }}
-</div>
+</article>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,7 +2,32 @@
 <x-layout>
     @foreach($jobs as $job)
         <x-card class="mb-4">
-            {{ $job->title }}
+            <div class="flex justify-between mb-2">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-sm text-slate-500">{{"£" . number_format($job->salary)}}</div>
+            </div>
+            <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
+                <div class="flex space-x-4">
+                    <div>
+                        Company Name
+                    </div>
+                    <div>
+                        {{ $job->location }}
+                    </div>
+                </div>
+                <div class="flex space-x-1 text-xs">
+                    <div class="rounded-md border px-2 py-1">
+                        {{ ucfirst($job->experience) }}
+                    </div>
+                    <div class="rounded-md border px-2 py-1">
+                        {{ $job->category }}
+                    </div>
+                </div>
+            </div>
+            <p>
+                {!! nl2br(e($job->description)) !!}
+            </p>
+
         </x-card>
     @endforeach
 </x-layout>
