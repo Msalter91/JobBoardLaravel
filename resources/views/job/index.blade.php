8e6da66e5ebdabf21ff4bf67eff8bda15007fccf
msalter <mike.salter@wundermanthompson.com>
2023-09-15 16:37:14 +0100

    added breadcrumbs and components

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,6 @@
 {{-- Uses out layout and card as a component that fits in the slot section of layout --}}
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
     @foreach($jobs as $job)
         <x-job-card class="mb-4" :job="$job">
                 <div>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
+{{--    We're going to pass the breadcrums an array of links to use so that the nav is configurable--}}
+<x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#' ]" />
     <x-job-card :job="$job"></x-job-card>
 </x-layout>
